e5c7f17a64681a825a6d4842bd6bafe528297050
HUANG-Hongjun <bakahook@gmail.com>
2021-03-27 21:22:19 +0100

    完整版（几乎
    所有功能基本实现，数值有待调试

--- main.c
+++ main.c
@@ -21,7 +21,6 @@ void init_UART(void);
 void direction_control(void);
 void UART_Send_String(char *pbuff);
 void init_SPI(void);
-void SPI_Send_char(unsigned char carac);
 
 
 
@@ -49,7 +48,9 @@ int main(void)
 	while(1);
 }
 
+
 //====================================interrupt============================================
+
 //TX interrupt
 #pragma vector=USCIAB0TX_VECTOR
 __interrupt void USCI0TX_ISR(void)
@@ -58,25 +59,16 @@ __interrupt void USCI0TX_ISR(void)
     IFG2&=~UCA0TXIFG;
 }
 
+
+
 //RX interrupt
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR(void)
 {
-    //-------UART
-    if (IFG2 & UCA0RXIFG){
-        //while (!(IFG2&UCA0TXIFG));
-        IFG2&=~UCA0RXIFG;                // USCI_A0 TX buffer ready?
-        //UCA0TXBUF = UCA0RXBUF;
-        direction_control();
-    }
-
-    //--------------- SPI
-    else if (IFG2 & UCB0RXIFG)
-    {
-        while( (UCB0STAT & UCBUSY) && !(UCB0STAT & UCOE) );
-        while(!(IFG2 & UCB0RXIFG));
-    }
-
+   //while (!(IFG2&UCA0TXIFG));
+   IFG2&=~UCA0RXIFG;                // USCI_A0 TX buffer ready?
+   //UCA0TXBUF = UCA0RXBUF;
+   direction_control();
 }
 
 
@@ -148,51 +140,22 @@ void init_UART(void){
 
       IE2 |= UCA0RXIE+UCA0TXIE;                 //enable interrupt
       _enable_interrupts();
-      _bis_SR_register(LPM0_bits);
+      //_bis_SR_register(LPM0_bits);
  }
 
 void init_SPI( void )
 {
-    // Waste Time, waiting Slave SYNC
-    __delay_cycles(250);
-
-    // SOFTWARE RESET - mode configuration
-    UCB0CTL0 = 0;
-    UCB0CTL1 = (0 + UCSWRST*1 );
-
-    // clearing IFg /16.4.9/p447/SLAU144j
-    // set by setting UCSWRST just before
-    IFG2 &= ~(UCB0TXIFG | UCB0RXIFG);
-
-    // Configuration SPI (voir slau144 p.445)
-    // UCCKPH = 0 -> Data changed on leading clock edges and sampled on trailing edges.
-    // UCCKPL = 0 -> Clock inactive state is low.
-    //   SPI Mode 0 :  UCCKPH * 1 | UCCKPL * 0
-    //   SPI Mode 1 :  UCCKPH * 0 | UCCKPL * 0  <--
-    //   SPI Mode 2 :  UCCKPH * 1 | UCCKPL * 1
-    //   SPI Mode 3 :  UCCKPH * 0 | UCCKPL * 1
-    // UCMSB  = 1 -> MSB premier
-    // UC7BIT = 0 -> 8 bits, 1 -> 7 bits
-    // UCMST  = 0 -> CLK by Master, 1 -> CLK by USCI bit CLK / p441/16.3.6
-    // UCMODE_x  x=0 -> 3-pin SPI,
-    //           x=1 -> 4-pin SPI UC0STE active high,
-    //           x=2 -> 4-pin SPI UC0STE active low,
-    //           x=3 -> i²c.
-    // UCSYNC = 1 -> Mode synchrone (SPI)
-    UCB0CTL0 |= ( UCMST | UCMODE_0 | UCSYNC );
-    UCB0CTL0 &= ~( UCCKPH | UCCKPL | UCMSB | UC7BIT );
+    UCB0CTL1 |= UCSWRST;
     UCB0CTL1 |= UCSSEL_2;
-
-    UCB0BR0 = 0x0A;     // divide SMCLK by 10
+    P1SEL |= (BIT5 | BIT6 | BIT7);
+    P1SEL2 |= (BIT5 | BIT6 | BIT7);
+    UCB0CTL0 &= ~(UCCKPH | UCCKPL);
+    UCB0CTL0 |= (UCMODE_0 | UCMST | UCSYNC);
+    UCB0CTL0 &= ~(UCMSB | UC7BIT);
+    UCB0BR0 = 0x0A;
     UCB0BR1 = 0x00;
-
-    // SPI : Fonctions secondaires
-    // MISO-1.6 MOSI-1.7 et CLK-1.5
-    // Ref. SLAS735G p48,49
-    P1SEL  |= ( SCK | DATA_OUT | DATA_IN);
-    P1SEL2 |= ( SCK | DATA_OUT | DATA_IN);
-
-    UCB0CTL1 &= ~UCSWRST;                                // activation USCI
+    UCB0CTL1 &= ~UCSWRST;
+    IFG2 &= ~UCB0RXIFG;
 }
 
 
@@ -202,9 +165,14 @@ void direction_control(){
     switch(com){
         case 'h':
             UART_Send_String("h----help\n\r");
-            UART_Send_String("w----run\n\r");
-            UART_Send_String("s----stop\n\r");
-            SPI_Send_char(0x31);
+            UART_Send_String("8----run\n\r");
+            UART_Send_String("5----stop\n\r");
+            UART_Send_String("2----back\n\r");
+            UART_Send_String("4----turn left\n\r");
+            UART_Send_String("6----turn right\n\r");
+            UART_Send_String("q----camera turn left\n\r");
+            UART_Send_String("e----camera turn right\n\r");
+            UART_Send_String("w----camera reposition\n\r");
             break;
         case '8':
             UART_Send_String("run\n\r");
@@ -233,7 +201,18 @@ void direction_control(){
             TA1CCR1 = 500;             //positif 0-1000
             TA1CCR2 = 500;             //positif 500-1000
             break;
-
+        case'q':
+            UART_Send_String("camera turn left\n\r");
+            UCB0TXBUF = 0x31;
+            break;
+        case'e':
+            UART_Send_String("camera turn right\n\r");
+            UCB0TXBUF = 0x32;
+            break;
+        case'w':
+            UART_Send_String("camera reposition\n\r");
+            UCB0TXBUF = 0x33;
+            break;
         default:
             UART_Send_String("Command does not exist, use 'h' for help\n\r");
             break;
@@ -253,9 +232,4 @@ void UART_Send_String(char *pbuff)
 }
 
 
-void SPI_Send_char(unsigned char data)
-{
-    while ((UCB0STAT & UCBUSY));   // attend que USCI_SPI soit dispo.
-    while(!(IFG2 & UCB0TXIFG)); // p442
-    UCB0TXBUF = data;         // Put character in transmit buffer
-}
+
